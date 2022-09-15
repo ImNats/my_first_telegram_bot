a6f308c03ebcd429ed9f4333befd03ce0bbb0433
Butova Anastasiia <as444541019@gmail.com>
2022-09-15 23:16:44 +0300

    Бот научился :
    	рассказывать, что он умеет делать: CommandHandler('my_skills', describe_my_skill)
    	прощаться: CommandHandler('exit', goodbye_user)
    	отвечать на вопросы, искать созвездия для планет: (CommandHandler('ask_planet', ask_planet) (ask_planet, find_planet),
    	оркестрировать входящие сообщения (what_do_next)

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
+from codeop import CommandCompiler
 import logging
+import ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
@@ -21,26 +23,146 @@ logging.basicConfig(
 #     }
 
 
-def greer_user(update,context):
+def user_in_log(update,context,command_name):
     # вывели в консоль программы сообщение
-    print('Пользователь нажал /start: ')
+    print(f'Пользователь нажал /{command_name}: ')
     print(update['message']['date'])
     print(update['message']['chat'])
-    # print('Пользователь нажал /start: '+update['message']['date']+': '+update['message']['chat'])
+
+
+def greer_user(update,context):
+    # вывели в консоль программы сообщение
+    command_name = 'start'
+    user_in_log(update,context,command_name)
 
     # вывели сообщение пользователю в telegram-чат
-    update.message.reply_text('Привет!!! Вы запустили мой телеграмм ботик!!!! \nКак тебя зовут?')
+    update.message.reply_text(
+        'Привет!!!'
+        + '\nА меня зовут ТэБоттик :)'
+        + '\nВы запустили мой телеграмм ботик!!!!'
+        # + '\nКак тебя зовут?'
+    )
+
+def goodbye_user(update,context):
+    # вывели в консоль программы сообщение
+    command_name = 'exit'
+    user_in_log(update,context,command_name)
+
+    # вывели сообщение пользователю в telegram-чат
+    update.message.reply_text(
+        'Пока!!!'
+        +'\nПриходи поболтаем еще :)'
+    )
 
 
-def talk_with_me (update, context):
+def describe_my_skill(update,context):
+    # вывели в консоль программы сообщение
+    command_name = 'my_skills'
+    user_in_log(update,context,command_name)
+
+    # вывели сообщение пользователю в telegram-чат
+    update.message.reply_text(
+        'Я умею пока не много: '
+        +'\n  * здороваться :)'
+        +'\n  * Могу угадать в каком созвездии была планета '
+        +'\n  * попробовать тебе ответить на вопрос '
+        +'\n  * повторять слова '
+        +'\n  * говорить пока :('
+    )
+
+
+def what_do_next(update, context):
+    if find_planet(update, context) is not None:
+        text_to_user = find_planet(update, context)
+    elif ask_me(update, context) is not None:
+        text_to_user = ask_me(update, context) 
+    elif talk_with_me_echo(update, context) is not None:
+        text_to_user = talk_with_me_echo(update, context)
+    update.message.reply_text(text_to_user)
+
+
+def ask_planet(update, context):
+    text = '''Классно!!! Расположение какой планеты ты хочешь узнать? 
+    Вот какие планеты я знаю: <code>
+        Меркурий | Mercury
+        Венера   | Venus
+        Марс     | Mars
+        Юпитер   | Jupiter
+        Сатурн   | Saturn
+        Уран     | Uranus
+        Нептун   | Neptune
+        Плутон   | Pluto
+        Солнце   | Sun
+        Луна     | Moon </code>
+    Введи в ответ название планеты: '''
+    update.message.reply_text(text, parse_mode='HTML')
+
+
+def find_planet(update, context):
+    user_planet = update.message.text
+    planet_not_found = False
+
+    if user_planet in {'Mercury','Меркурий'}:
+        planet_id=ephem.Mercury('2022/09/19')
+    elif user_planet in {'Venus','Венера'}:
+        planet_id=ephem.Venus('2022/09/19')
+    elif user_planet in {'Mars','Марс'}:
+        planet_id=ephem.Mars('2022/09/19')
+    elif user_planet in {'Jupiter','Юпитер'}:
+        planet_id=ephem.Jupiter('2022/09/19')
+    elif user_planet in {'Saturn','Сатурн'}:
+        planet_id=ephem.Saturn('2022/09/19')
+    elif user_planet in {'Uranus','Уран'}:
+        planet_id=ephem.Uranus('2022/09/19')
+    elif user_planet in {'Neptune','Нептун'}:
+        planet_id=ephem.Neptune('2022/09/19')
+    elif user_planet in {'Pluto','Плутон'}:
+        planet_id=ephem.Pluto('2022/09/19')
+    elif user_planet in {'Sun','Солнце'}:
+        planet_id=ephem.Sun('2022/09/19')
+    elif user_planet in {'Moon','Луна'}:
+        planet_id=ephem.Moon('2022/09/19')
+    else:
+        planet_not_found = True
+    
+    if planet_not_found:
+        text_to_user = None
+    else:
+        text_to_user = 'Она была в созвездии '+ephem.constellation(planet_id)[1]
+    
+    return text_to_user
+
+
+questions_and_answers = {
+    'Привет': 'Привет!',
+    'Привет!': 'Привет!',
+    'Пока': 'Пока!',
+    'Пока!': 'Пока!',
+    'Как дела?': 'Хорошо!',
+    'Как жизнь?': 'Хорошо!',
+    'Что делаешь?': 'Программирую',
+    'Какая завтра погода?': 'Дождливая :(',
+    'Что ты умеешь?': 'Отвечать на вопросы!',
+    'Что умеешь?': 'Отвечать на вопросы!',
+    'Завтра будет гроза?': 'Сейчас не май, не будет!',
+    'Как провел лето?': 'Валялся на пляже!'
+}
+
+
+def ask_me(update, context):
+    text_from_user = update.message.text
+    if questions_and_answers.get(text_from_user) is not None:
+        print(text_from_user)
+        text_to_user = questions_and_answers[text_from_user]
+        return text_to_user
+
+
+def talk_with_me_echo(update, context):
     text_from_user = update.message.text
     text_to_user = text_from_user
     print(text_to_user)
-    update.message.reply_text(
-        'Привет, '+text_to_user+'!!!'
-        +'\nА меня зовут ТэБоттик :)'
-        +'\nПока это все что я умею...'
-        )
+    text_to_user = 'Я знаю ты сказал: ' + text_to_user + '!!!' + '\nОткрой меню и я расскажу, чему еще я научилась'
+    return text_to_user
 
 # здесь будет лежать тело бота
 def main():
@@ -53,7 +175,12 @@ def main():
 
     # настраиваем диспетчера:
     mybot.dispatcher.add_handler(CommandHandler('start', greer_user))
-    mybot.dispatcher.add_handler(MessageHandler(Filters.text, talk_with_me))
+    mybot.dispatcher.add_handler(CommandHandler('exit', goodbye_user))
+    mybot.dispatcher.add_handler(CommandHandler('my_skills', describe_my_skill))
+    mybot.dispatcher.add_handler(CommandHandler('ask_planet', ask_planet))
+
+    mybot.dispatcher.add_handler(MessageHandler(Filters.text, what_do_next))
+
 
     logging.info('telegram bot starting')
     # заставили бота запрашивать с сервера телеграмма все входящие в него сообщения
